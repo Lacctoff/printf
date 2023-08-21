efd9e13079197a1dbd85885b59daff6bd4b5fefc
Lacctoff <wolilacctoff@gmail.com>
2023-08-21 05:27:21 -0700

    this function prints a string

---
+++ _strputss.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strputss - prints a string followed by a new line to the stdout.
+ * @str: input string.
+ * Return: output count of the string as an integer.
+ */
+
+int _strputss(char *str)
+{
+	int outputCount = 0;
+
+	for (outputCount = 0; str[outputCount] != '\0'; outputCount++)
+	{
+		_writechar(str[output]);
+	}
+	return (outputCount);
+}
