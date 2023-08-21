8de5812d39c2433619b828f7524065ca363172aa
Lacctoff <wolilacctoff@gmail.com>
2023-08-21 06:34:26 -0700

    custom printf function

---
+++ _printf.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _printf - It processes a format string and a variable set of arguments,
+ * then formats and outputs the result
+ * accordingly.
+ *
+ * @format: used to specify the desired output format
+ * Return: returns the number of characters printed.
+*/
+int _printf(const char *format, ...);
+
+int _printf(const char *format, ...)
+{
+	va_list argList;
+	unsigned int i;
+	unsigned int count;
+	int bites_count;
+
+	count = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+
+	va_start(argList, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_writechar(format[i]);
+			count++;
+		}
+		else
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					_writechar(va_arg(argList, int));
+					count++;
+					i++;
+					break;
+				case 's':
+					bites_count = _strputss(va_arg(argList, char *));
+					i += 1;
+					count = (count + (bites_count - 1));
+					break;
+				case '%':
+					_writechar('%');
+					count++;
+					break;
+				default:
+					break;
+
+			}
+		}
+	}
+	va_end(argList);
+	return (count);
+}
