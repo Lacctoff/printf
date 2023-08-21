b59c63aa29cc3df585b495c2d9c99b736b029fa8
titosco <titobiloluwatella@gmail.com>
2023-08-21 13:23:07 -0700

    printf

--- _numput.c
+++ _numput.c
@@ -1,4 +1,8 @@
 #include "main.h"
+
+int _itoa(int num, char *buffer);
+void _reverse(char *str, int length);
+void _write(char *str, int length);
 /**
  * _numput - Prints an integer to the standard output
  * @num: The integer to be printed
@@ -7,10 +11,64 @@
  */
 int _numput(int num)
 {
-	char buffer[20]; // Assuming an upper limit on int digits
-	int length = snprintf(buffer, sizeof(buffer), "%d", num);
+	char buffer[20];
+	int length = _itoa(num, buffer);
 
-	write(1, buffer, length);
+	_write(buffer, length);
+	return (length);
+}
+
+/**
+ * _itoa - Convert an integer to a string
+ * @num: The integer to be converted
+ * @buffer: The buffer to store the result string
+ *
+ * Return: The length of the converted string
+ */
+int _itoa(int num, char *buffer)
+{
+	int length = 0;
+	int is_negative = 0;
+
+
+	if (num < 0)
+	{
+		is_negative = 1;
+		num = -num;
+		length++;
+	}
+
+	do {
+		buffer[length++] = '0' + (num % 10);
+		num /= 10;
+	} while (num > 0);
+
+
+	if (is_negative)
+	{
+		buffer[length++] = '-';
+	}
+
+	_reverse(buffer, length);
 
 	return (length);
 }
+
+/**
+ * _reverse - Reverse a string in-place
+ * @str: The string to be reversed
+ * @length: The length of the string
+ */
+void _reverse(char *str, int length)
+{
+	int i, j;
+	char temp;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
+}
+
--- main.h
+++ main.h
@@ -10,4 +10,8 @@ int _printf(const char *format, ...);
 int _writechar(char c);
 int _strputss(char *str);
 int _numput(int num);
+
+int _itoa(int num, char *buffer);
+void _reverse(char *str, int length);
+void _write(char *str, int length);
 #endif
