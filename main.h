6f0d87201f33938952ef5048642897fee90985ab
Lacctoff <wolilacctoff@gmail.com>
2023-08-21 05:04:04 -0700

    protype files and headers

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+int _printf(const char *format, ...);
+int _writechar(char c);
+int _strputss(char *str);
+
+#endif
